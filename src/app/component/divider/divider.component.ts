035326c9cc7d7a8f3e7c881bcace2b1eabb7c81f
Michael Stifter <75159397+neonalps@users.noreply.github.com>
2025-06-07 17:43:59 +0200

    feat: improve game event styles (#22)

--- src/app/component/divider/divider.component.ts
+++ src/app/component/divider/divider.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-divider',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './divider.component.html',
   styleUrl: './divider.component.css'
 })
 export class DividerComponent {
 
   @Input() text!: string;
+  @Input() bgColor: string | undefined;
+  @Input() textSize: string | undefined;
+  @Input() isTransparent = false;
+  @Input() additionalClasses: string | undefined;
+
+  getBgColor(): string {
+    return this.bgColor ?? "bg-color-dark-grey-darker";
+  }
+
+  getTextSize(): string {
+    return this.textSize ?? "text-sm";
+  }
+
+  getAdditionalClasses(): string {
+    return this.additionalClasses ?? "";
+  }
 
 }
