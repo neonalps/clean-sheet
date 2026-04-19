2036ab07e792d6bd65172c44680ed9c49df39a8d
Michael Stifter <michael@neonalps.at>
2026-04-19 19:54:00 +0200

    feat: add manager period duration display

--- src/app/component/manager-period/manager-period.component.ts
+++ src/app/component/manager-period/manager-period.component.ts
@@ -1,23 +1,38 @@
-import { CommonModule } from '@angular/common';
-import { Component, input, OnInit, signal } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { ManagerPeriod } from '@src/app/model/manager';
-import { I18nPipe } from '@src/app/module/i18n/i18n.pipe';
 import { UiIconComponent } from "@src/app/component/ui-icon/icon.component";
 import { getPersonName } from '@src/app/util/domain';
+import { TranslationService } from '@src/app/module/i18n/translation.service';
+import { ensureNotNullish, isNotDefined } from '@src/app/util/common';
 
 @Component({
   selector: 'app-manager-period',
-  imports: [CommonModule, I18nPipe, UiIconComponent],
+  imports: [CommonModule, UiIconComponent],
   templateUrl: './manager-period.component.html',
 })
 export class ManagerPeriodComponent implements OnInit {
 
-  readonly personName = signal<string>('');
+  readonly durationText = signal('');
+  readonly personName = signal('');
 
   readonly period = input.required<ManagerPeriod>();
 
+  private readonly datePipe = new DatePipe('en-US');
+  private readonly translationService = inject(TranslationService);
+
   ngOnInit(): void {
-    this.personName.set(getPersonName(this.period().person));
+    const periodValue = this.period();
+    this.personName.set(getPersonName(periodValue.person));
+
+    const startDate = ensureNotNullish(this.datePipe.transform(new Date(periodValue.start)));
+    const endDate = periodValue.end ? this.datePipe.transform(new Date(periodValue.end)) : undefined;
+
+    if (isNotDefined(periodValue.end)) {
+      this.durationText.set(this.translationService.translate('managerPeriod.since', { since: startDate }));
+    } else {
+      this.durationText.set(this.translationService.translate('managerPeriod.fromTo', { from: startDate, to: ensureNotNullish(endDate) }));
+    }
   }
 
 }
