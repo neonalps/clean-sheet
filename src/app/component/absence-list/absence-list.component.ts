14e8a2c14a31c568a7bdf1404a4c5bce268f4679
Michael Stifter <michael@neonalps.at>
2026-04-26 09:03:48 +0200

    feat: improve game absences

--- src/app/component/absence-list/absence-list.component.ts
+++ src/app/component/absence-list/absence-list.component.ts
@@ -1,9 +1,13 @@
-import { Component, input, OnInit, signal } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { GameAbsence, GameAbsenceType } from '@src/app/model/game';
 import { GameAbsenceComponent } from "@src/app/component/game-absence/game-absence.component";
 import { CommonModule } from '@angular/common';
 import { assertUnreachable } from '@src/app/util/common';
 import { I18nPipe } from '@src/app/module/i18n/i18n.pipe';
+import { Person } from '@src/app/model/person';
+import { navigateToPerson } from '@src/app/util/router';
+import { Router } from '@angular/router';
+import { getDisplayName } from '@src/app/util/domain';
 
 @Component({
   selector: 'app-absence-list',
@@ -18,6 +22,8 @@ export class AbsenceListComponent implements OnInit {
   readonly injured = signal<GameAbsence[]>([]);
   readonly atRisk = signal<GameAbsence[]>([]);
   readonly other = signal<GameAbsence[]>([]);
+
+  private readonly router = inject(Router);
 
   ngOnInit(): void {
     for (const absence of this.absences()) {
@@ -39,6 +45,10 @@ export class AbsenceListComponent implements OnInit {
           assertUnreachable(absenceType);
       }
     }
+  }
+
+  onPersonClicked(person: Person) {
+    navigateToPerson(this.router, person.id, getDisplayName(person.firstName, person.lastName));
   }
 
 }
--- src/app/component/game-absence/game-absence.component.ts
+++ src/app/component/game-absence/game-absence.component.ts
@@ -1,9 +1,11 @@
-import { Component, input, OnInit, signal } from '@angular/core';
+import { Component, inject, input, OnInit, output, signal } from '@angular/core';
 import { GameAbsence, GameAbsenceType } from '@src/app/model/game';
 import { PersonCardComponent } from "@src/app/component/person-card/person-card.component";
 import { UiIconComponent } from "@src/app/component/ui-icon/icon.component";
 import { CommonModule } from '@angular/common';
 import { camelToKebab } from '@src/app/util/common';
+import { TranslationService } from '@src/app/module/i18n/translation.service';
+import { Person } from '@src/app/model/person';
 
 type SuspensionCause = 'yellowCard' | 'redCard' | 'yellowRedCard';
 
@@ -21,7 +23,12 @@ export class GameAbsenceComponent implements OnInit {
 
   readonly absence = input.required<GameAbsence>();
 
+  readonly onPersonClick = output<Person>();
+
+  readonly reasonText = signal<string>('');
   readonly suspension = signal<Suspension | null>(null);
+
+  private readonly translationService = inject(TranslationService);
 
   ngOnInit(): void {
     const absenceValue = this.absence();
@@ -34,7 +41,17 @@ export class GameAbsenceComponent implements OnInit {
       case GameAbsenceType.Suspended:
         this.suspension.set({ cause: camelToKebab(absenceReasonParts[0]) as SuspensionCause, quantity: absenceReasonParts[1]});
         break;
+      case GameAbsenceType.Injured:
+        this.reasonText.set(this.translationService.translate(`injury.${absenceReasonParts[0]}`));
+        break;
+      case GameAbsenceType.Exempt:
+        this.reasonText.set(this.translationService.translate(`absence.${absenceReasonParts[0]}`));
+        break;
     }
+  }
+
+  personClicked() {
+    this.onPersonClick.emit(this.absence().person);
   }
 
 }
